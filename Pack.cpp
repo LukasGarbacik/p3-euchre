c56e23f5be6081f6f3c96ce1d1a70122137337a0
Lukas Garbacik <lukasg@umich.edu>
2024-02-17 15:22:12 -0500

    pack publics working

--- Pack.cpp
+++ Pack.cpp
@@ -1,17 +1,15 @@
 #include <iostream>
 #include <cassert>
 #include "Pack.hpp"
-
-
-
+#include "Card.hpp"
 
 using namespace std;
 
 
 Pack::Pack(){
    int count = 0;
-   for(size_t i = 0; i <= 3; i++){
-       for(size_t j = 7; j <= 12; j++){
+   for(size_t i = SPADES; i <= DIAMONDS; i++){
+       for(size_t j = NINE; j <= ACE; j++){
            assert(count < PACK_SIZE);
            cards[count] = Card(static_cast<Rank>(j),static_cast<Suit>(i));
            count++;
@@ -19,7 +17,6 @@ Pack::Pack(){
    }
    next = 0;
 }
-
 
 Pack::Pack(istream &pack_input){
     int count = 0;
@@ -41,25 +38,30 @@ void Pack::reset(){
    next = 0;
 }
 
-
 void Pack::shuffle(){
-   int localIndex;
-   for(size_t i = 0; i < 7; ++i){
-       localIndex = 0;
-       Card newDeck [PACK_SIZE];
+    for(size_t i = 0; i < 7; ++i){
+       array<Card, PACK_SIZE/2> Top;
+       array<Card, PACK_SIZE/2> Bottom;
        for(size_t j = 0; j < PACK_SIZE/2; ++j){
-           newDeck[localIndex] = cards[localIndex + PACK_SIZE/2];
-           localIndex++;
-           newDeck[j] = cards[localIndex];
-           localIndex++;
+        Top[j] = cards[j];
        }
-       for(int k = 0; k < PACK_SIZE; ++k)
-       {
-        cards[k] = newDeck[k];
+       for(size_t k = 0; k < PACK_SIZE/2; ++k){
+        Bottom[k] = cards[k + PACK_SIZE/2];
        }
-   }
-}
-
+       int topIndex = 0;
+       int bottomIndex = 0;
+       for(size_t m = 0; m < PACK_SIZE; ++m){
+        if(m % 2 == 0){
+            cards[m] = Bottom[bottomIndex];
+            bottomIndex++;
+        }
+        else{
+            cards[m] = Top[topIndex];
+            topIndex++;
+        }
+       }
+    }
+} 
 
 bool Pack::empty() const{
    if(next >= 24){
